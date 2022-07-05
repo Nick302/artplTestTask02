72eb08847c622e0bc04b1ddc963882c209232f57
Cold <38143140+Nick302@users.noreply.github.com>
2022-07-05 23:50:39 +0200

    create project

---
+++ src/main/java/com/example/artpltesttask02/ArtplTestTask02Application.java
@@ -0,0 +1,13 @@
+package com.example.artpltesttask02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtplTestTask02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtplTestTask02Application.class, args);
+    }
+
+}
